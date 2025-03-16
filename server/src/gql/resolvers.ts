a5c761a1da8613d9844f00a2c93471b1a3322e70
JN3141 <justin.ng.3141@gmail.com>
2025-03-16 17:51:11 +1100

    NO-ISSUE Fix small typo

--- server/src/gql/resolvers.ts
+++ server/src/gql/resolvers.ts
@@ -1,28 +1,28 @@
 import feedbackStore from "../store/feedback";
-import feeedbackService from "../service/feedback";
+import feedbackService from "../service/feedback";
 
 /**
  * GraphQL Resolvers
  */
 const resolvers = {
   Query: {
-    feedback: (parent: unknown, args: {id: number}) => {
-      return feedbackStore.getFeedback(args.id)
+    feedback: (parent: unknown, args: { id: number }) => {
+      return feedbackStore.getFeedback(args.id);
     },
     feedbacks: (parent: unknown, args: { page: number; per_page: number }) => {
-      return feeedbackService.getFeedbackPage(args.page, args.per_page)
+      return feedbackService.getFeedbackPage(args.page, args.per_page);
     },
   },
   Mutation: {
     createFeedback: (parent: unknown, args: { text: string }) => {
-      return feeedbackService.createFeedback(args.text)
-    }
+      return feedbackService.createFeedback(args.text);
+    },
   },
   Feedback: {
     highlights: () => {
-      return []
-    }
-  }
+      return [];
+    },
+  },
 };
 
 export default resolvers;
